c37e288a45062dcde5119a1e2939fed0e85eba98
Vineeth Ravindra <vineeth89@gmail.com>
2016-11-28 13:45:46 -0500

    Read UserName and password from console to authenticate user

--- Client/client.py
+++ Client/client.py
@@ -7,7 +7,7 @@ class client:
         print "********************************"
         print "Welcome to the secure terminal"
         print "have a fun time :)"
-        print "********************************\n\n"
+        print "********************************\n"
 
     def run(self):
         '''
@@ -19,19 +19,40 @@ class client:
                           Tries to establish connection with server, authenticates user
                           if user is authenticated on specific port to active connections
         '''
-        while True:
-            # print "Enter Username :"
-            # userName = sys.stdin.readline()
-            # print "Enter Password :"
-            # password = sys.stdin.readline()
-            c = serverConnection.connection("alice","password").establishConnection()
-            sys.exit(0)
-            if c:
-                break
-            else :
-                print "You know the drill ;) good luck this time!\n"
-        self.__serverObj = c
-        self.__listenForInputs()
+        self.__serverObj = self.__authenticateUser()
+
+
+
+    def __authenticateUser(self):
+        '''
+        __authenticateUser(None) :
+            Input   : None
+            Output  : Object (Instance of connection to server)
+            Purpose : To authenticate user and set up connection with server
+        '''
+
+        userName = self.__readFromConsole("Enter Username: ").rstrip()
+        password = self.__readFromConsole("Enter Password: ").rstrip()
+        c = serverConnection.connection(userName, password).establishConnection()
+        if c:
+            return c
+        else :
+            self.__authenticateUser()
+
+    def __readFromConsole(self,message):
+        '''
+                __readFromConsole(String) :
+                    Input   : Message to be printed on screen
+                    Output  : The string entered on console
+                    Purpose : Write a message on console and read from same
+                '''
+        sys.stdout.write(message)
+        sys.stdout.flush()
+        inputStreams = [sys.stdin]
+        ready_to_read, ready_to_write, in_error = \
+            select.select(inputStreams, [], [])
+        msg = sys.stdin.readline()
+        return msg
 
     def __listenForInputs(self):
         '''
